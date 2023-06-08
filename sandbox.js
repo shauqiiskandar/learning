1c4682c0032b75da0e94e441f1ff739c9c570006
Shauqi Iskandar <iquahs@gmail.com>
2023-06-08 10:19:41 +0800

    returning Boolean Values from Functions

--- sandbox.js
+++ sandbox.js
@@ -1,27 +1,9 @@
-function chainToSwitch(val) {
-  let answer = "";
+function isLess(a, b) {
   // Only change code below this line
 
-  switch (val) {
-    case "bob":
-      answer = "Marley";
-      break;
-    case 42:
-      answer = "The Answer";
-      break;
-    case 1:
-      answer = "There is no #1";
-      break;
-    case 99:
-      answer = "Missed me by this much!";
-      break;
-    case 7:
-      answer = "Ate Nine";
-      break;
-  }
+  return a < b;
 
   // Only change code above this line
-  return answer;
 }
 
-chainToSwitch(7);
+isLess(10, 15);
